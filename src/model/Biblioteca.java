8db58392b9c5b8ff9e53b95904fe353a9f39b245
Guilherme Mendonça <Guilhermep.m.180@gmail.com>
2025-06-12 21:36:11 -0300

    Create Biblioteca.java

---
+++ src/model/Biblioteca.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca {
+    public List<Usuario> usuarios = new ArrayList<>();
+    public List<Livro> livros = new ArrayList<>();
+    public List<Emprestimo> emprestimos = new ArrayList<>();
+}
+
+
