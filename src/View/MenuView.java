464e2e56b85b12e0a02ef0855169988436175d53
Guilherme Mendonça <Guilhermep.m.180@gmail.com>
2025-06-13 14:03:48 -0300

    Update MenuView.java

--- src/View/MenuView.java
+++ src/View/MenuView.java
@@ -1,20 +1,22 @@
 package view;
 
 import java.util.Scanner;
+import model.Usuario;
+import model.Livro;
 
 public class MenuView {
     private Scanner scanner = new Scanner(System.in);
 
     public int exibirMenuPrincipal() {
-        System.out.println("\n1. Cadastrar Usuário");
+        System.out.println("\n--- Menu Principal da Biblioteca ---"); // Adicionado um título para o menu
+        System.out.println("1. Cadastrar Usuário");
         System.out.println("2. Cadastrar Livro");
         System.out.println("3. Realizar Empréstimo");
-        System.out.println("4. Devolver Livro");
-        System.out.println("5. Listar Usuários");
-        System.out.println("6. Listar Livros");
-        System.out.println("7. Sair");
+        System.out.println("4. Listar Usuários");
+        System.out.println("5. Listar Livros");
+        System.out.println("6. Deletar Usuário"); // Nova opção
+        System.out.println("7. Sair"); // Opção de sair movida para baixo
         System.out.print("Escolha uma opção: ");
         return scanner.nextInt();
     }
 }
-
